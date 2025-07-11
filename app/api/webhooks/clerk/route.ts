f7b7a09c1c4578e9f23b86b7760e72edb891acf4
Mikielai <151890791+Mikielai@users.noreply.github.com>
2025-07-11 21:09:25 +0800

    pls

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -42,17 +42,27 @@ export async function POST(req: Request) {
   const eventType = evt.type
 
   if (eventType === 'user.created') {
-    const { id, email_addresses, first_name, last_name } = evt.data
-
-    await db.user.create({
-      data: {
-        id: id,
-        email: email_addresses[0].email_address,
-        firstName: first_name || '',
-        lastName: last_name || '',
-      },
-    })
+    const { id, email_addresses, first_name, image_url, last_name } = evt.data
+    try {
+      const newEvent = await db.user.create({
+        data: {
+          id: id, // ✅ This uses the id variable
+          email: email_addresses[0].email_address,
+          firstName: first_name,
+          lastName: last_name,
+          profileimage: image_url,
+        },
+      })
+      return new Response(JSON.stringify(newEvent), {
+        status: 201,
+      })
+    } catch (err) {
+      console.error('Error: Failed to store event in the database:', err)
+      return new Response('Error: Failed to store event in the database', {
+        status: 500,
+      })
+    }
   }
 
-  return new NextResponse('', { status: 200 })
+  return new Response('Webhook received', { status: 200 })
 }
--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 
 import Container from "@/components/container";
 import ImageUpload from "@/components/ImageUpload";
